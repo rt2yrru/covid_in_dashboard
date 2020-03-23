555b758e54a0b0d0942b3699e65e368bcb9a3c0d
siddht4 <dhar.75.ap@gmail.com>
2020-03-23 20:24:36 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,13 @@
 _url='https://corona.lmao.ninja/countries/india'
+fetch('_url)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  console.log('Error')
+  })
